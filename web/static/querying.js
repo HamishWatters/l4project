fe64fa0d4b1a0da1ea86f0ab8d94cca0f0d2ade6
HamishWatters <hamishw13@gmail.com>
2018-01-17 14:39:28 +0000

    Remembered javascript this time, and added some bootstraps on the first box

---
+++ web/static/querying.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(".add-heading-btn").click(function() {
+    $("#start").append("<div class=\"query-box\"><input class=\"title-box\" type=\"text\" name=\"titleBox\" placeholder = \"Heading\"></div>");
+  });
+});
