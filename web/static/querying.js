b1d24dcc683454e38827a6cb7ca403dbce0c82bf
hamish <hamishw13@gmail.com>
2018-01-18 22:30:16 +0000

    Constructing nested headings with the web ui working as I'd hope it to be. CSS and HTML need some serious refactoring, but it at least works correctly

--- web/static/querying.js
+++ web/static/querying.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-  $(".add-heading-btn").click(function() {
-    $("#start").append("<div class=\"query-box\"><input class=\"title-box\" type=\"text\" name=\"titleBox\" placeholder = \"Heading\"></div>");
-  });
+    $(".add-top-heading-btn").on("click", function() {console.log("Hello");
+        $(this).parents().eq(2).append("<div class=\"nested-query-box\"><div class=\"col-lg-6\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div></div>");
+    });
+
+    $(document).on("click", ".add-heading-btn", function() {
+        $(this).parents().eq(2).append("<div class=\"nested-query-box\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-nested-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div>");
+    });
+
+    $(document).on("click", ".delete-heading-btn", function() {
+        $(this).parents().eq(3).remove();
+    });
+    $(document).on("click", ".delete-nested-heading-btn", function() {
+        $(this).parents().eq(2).remove();
+    });
 });
