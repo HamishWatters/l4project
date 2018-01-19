8a93b53c9d148f0fe58b5c52bc7e4209bc8e428f
hamish <hamishw13@gmail.com>
2018-01-19 09:19:21 +0000

    Edited the JS object produced to an array using identifiers rather than the text of the box, to avoid problems with duplicate heading strings.

--- web/static/querying.js
+++ web/static/querying.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var nextIdentifier = 1;
-    $(".add-top-heading-btn").on("click", function() {console.log("Hello");
+    $(".add-top-heading-btn").on("click", function() {
         $(this).parents().eq(2).append("<div class=\"nested-query-box\" data-identifier=\""+(nextIdentifier++)+"\" data-parent=\"0\"><div class=\"col-lg-6\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div></div>");
     });
 
@@ -18,15 +18,15 @@ $(document).ready(function() {
     $(document).on("click", ".search-btn", function() {
         var root = $(".query-box");
         var headings = root.find(".nested-query-box");
-        var headingsObject = {};
-        headingsObject[$(".title").val()] = "root";
+        var headingsObject = [];
+        headingsObject[0] = [$(".title").val(),"root"];
         for (i=0; i<headings.length;i++) {
             var child = headings[i]
             while (child.value == undefined && child.children != undefined) {
                 child = child.children[0];
             }
             if (child.value == undefined) child = child.children[0];
-            headingsObject[child.value] = [headings[i].getAttribute("data-identifier"),headings[i].getAttribute("data-parent")];
+            headingsObject[headings[i].getAttribute("data-identifier")] = [child.value,headings[i].getAttribute("data-parent")];
         }
         console.log(headingsObject);
     });
