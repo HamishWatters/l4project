5b5f55de20dd6b0b8ce018f30e8260f17759adb8
Hamish <hamishw13@gmail.com>
2018-01-26 22:14:28 +0000

    Modifications to UI

--- web/static/querying.js
+++ web/static/querying.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
     var nextIdentifier = 1;
     $(".add-top-heading-btn").on("click", function() {
-        $(this).parents().eq(0).append("<div class=\"nested-query-box\" data-identifier=\""+(nextIdentifier++)+"\" data-parent=\"0\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div>");
+        $(this).parent().append("<div class=\"nested-query-box\" data-identifier=\""+(nextIdentifier++)+"\" data-parent=\"0\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div>");
     });
 
     $(document).on("click", ".add-heading-btn", function() {
         var parent = $(this).parents('div.nested-query-box');
-        $(this).parents().eq(2).append("<div class=\"nested-query-box\" data-identifier=\""+(nextIdentifier++)+"\" data-parent=\""+parent.data('identifier')+ "\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-nested-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div>");
+        $(this).parents().eq(2).append("<div class=\"nested-query-box\" data-identifier=\""+(nextIdentifier++)+"\" data-parent=\""+parent.data('identifier')+ "\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"Heading\"><span class=\"input-group-btn\"><button class=\"add-heading-btn btn btn-secondary\"><i class=\"fa fa-plus\"></i></button></span><span class=\"input-group-btn\"><button class=\"delete-heading-btn btn btn-secondary\"><i class=\"fa fa-minus\"></i></button></span></div></div>");
     });
 
     $(document).on("click", ".delete-heading-btn", function() {
-        $(this).parents().eq(3).remove();
-    });
-    $(document).on("click", ".delete-nested-heading-btn", function() {
         $(this).parents().eq(2).remove();
     });
     $(document).on("click", ".search-btn", function() {
